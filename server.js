5473845076154c0af32107a7fa74da35e5ce9cc8
SonHanSol1 <gksthf7869@naver.com>
2022-11-03 15:28:50 +0900

    express and mongoDB connected

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const PORT = process.env.PORT || 4000;
+import express from 'express'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
 
-app.use(cors());
+import index from "./app/routes/index.js"
 
-app.get("/", (req,res)=>{
-    res.send("now Path = '/'");
-})
+import db from './app/models/index.js'
 
-app.listen(PORT, ()=>{
-    console.log(`LAMAH BACK_END SERVER RUNNING : http://localhost:${PORT}`);
-})
+import applyDotenv from './app/lambdas/applyDotenv.js'
+
+
+async function startServer(){
+    const app = express()
+    const {mongoUri, port, jwtSecret} = applyDotenv(dotenv)
+    app.use(express.static('public'));
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json());
+    app.use("/", index);
+    app.use(morgan('dev'))
+    db
+    .mongoose
+    .connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('몽고DB 연결 성공')
+    })
+    .catch(err => {
+        console.log('몽고DB와 연결 실패', err)
+        process.exit();
+    });
+
+    app.listen(27017, () => {
+        console.log('***************** ***************** *****************')
+        console.log('********** 서버가 정상적으로 실행되고 있습니다 *********')
+        console.log('***************** ***************** *****************')
+    })
+}
+startServer()
