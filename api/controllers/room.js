f32dade845bd203f0d99709be0b4912f92801727
chowlrma <96705927+chowlrma@users.noreply.github.com>
2022-11-14 17:01:30 +0900

    ㅁㅁㅁ

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -2,23 +2,21 @@ import Room from "../models/Room.js";
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
 
-export const createRoom = async (req, res, next)=>{
-
+export const createRoom = async (req, res, next) => {
     const hotelId = req.params.hotelid;
-    const newRoom = new Room(req.body)
-
+    const newRoom = new Room(req.body);
     try {
-        const savedRoom = await newRoom.save()
-        try {
-            await Hotel.findByIdAndUpdate(hotelId, {
-                $push : {rooms: savedRoom._id},
-            });
-        } catch (err) {
-            next(err);
-        }
-        res.status(200).json(savedRoom);
-    } catch (err) {
+      const savedRoom = await newRoom.save();
+      try {
+        await Hotel.findByIdAndUpdate(hotelId, {
+          $push: { rooms: savedRoom._id },
+        });
+      } catch (err) {
         next(err);
+      }
+      res.status(200).json(savedRoom);
+    } catch (err) {
+      next(err);
     }
 };
 
@@ -32,18 +30,22 @@ export const updateRoom = async (req, res, next)=>{
         );
         res.status(200).json(updateRoom); 
     } catch (err) {
-        next(err)
+        next(err);
     }
 }
 
 export const deleteRoom = async (req, res, next)=>{
+    const hotelId = req.params.hotelid;
     try {
         await Room.findByIdAndDelete(
             req.params.id
         );
-        res.status(200).json("Room has been deleted.")
+        await Hotel.findByIdAndUpdate(hotelId, {
+            $pull: { rooms: req.params.id},
+        });
+        res.status(200).json("Room has been deleted.");
     } catch (err) {
-        next(err)
+        next(err);
     }
 }
 
@@ -52,9 +54,9 @@ export const getRoom = async (req, res, next)=>{
         const room = await Room.findById(
             req.params.id
         );
-        res.status(200).json(room)
+        res.status(200).json(room);
     } catch (err) {
-        next(err)
+        next(err);
     }
 }
 
@@ -63,6 +65,6 @@ export const getRooms = async (req, res, next)=>{
         const rooms = await Room.find();
         res.status(200).json(rooms)
     } catch (err) {
-        next(err)
+        next(err);
     }
 }
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,18 +1,26 @@
 import express from "express";
-import { createRoom, updateRoom, deleteRoom, getRoom, getRooms } from "../controllers/room.js";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+} from "../controllers/room.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
-
-//CREATE 
+//CREATE
 router.post("/:hotelid", verifyAdmin, createRoom);
+
 //UPDATE
 router.put("/:id", verifyAdmin, updateRoom);
 //DELETE
-router.delete("/:id", verifyAdmin, deleteRoom);
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
 //GET
-router.get("/:id",  getRoom);
+
+router.get("/:id", getRoom);
 //GET ALL
+
 router.get("/", getRooms);
 
-export default router
+export default router;
